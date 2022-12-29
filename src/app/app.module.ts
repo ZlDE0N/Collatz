dc19033ffe35d1b749fbccd7d43ffa1ce54aa3d3
Salvador Pereira <Ramon.Pereirae@gmail.com>
2022-12-28 21:06:26 -0600

    Collatz conjeture project version 1.0.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { HighchartComponent } from './highchart/highchart.component';
+import { AddComponent } from './add/add.component';
+import { AsideComponent } from './aside/aside.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    HighchartComponent,
+    AddComponent,
+    AsideComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
