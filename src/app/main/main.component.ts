64cf5f56ff846e30c7f51af14efdda13695740ff
Salvador Pereira <Ramon.Pereirae@gmail.com>
2023-01-04 14:27:14 -0600

    Collatz conjeture project version 1.2.2

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -12,10 +12,39 @@ export class MainComponent {
   NumberValue:any;
   array_values:any =[];
   store = [] as any;
+  // store:[] = []
+
+
+
+
+
+  // const collatzTail = (num: number, storage:number[] = store): any => {
+  //   //if num is 1 then store 1
+  //   if(num === 1) { 
+  //       storage.push(1);
+  //       return store;
+
+  //   //if num is even then store num / 2
+  //   } else if(num % 2 === 0) {
+  //       storage.push(num); 
+  //       return collatzTail((num / 2), store);
+  //     //if num is odd then store num * 3 + 1
+
+  //   } else {
+  //       storage.push(num); 
+  //       return collatzTail(3 * num + 1, store);
+  //   }
+  // };
+
 
   test(e:any){
     console.log(e) //Esto viene desde un output del add-component
     console.log('lo de arriba es por que lo hiciste bien!')
+    if(e === 1){
+      this.store.push(1)
+      return this.store
+    }
+    console.log(this.store)
   }
 
   onDelete(){
