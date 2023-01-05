4e48faec3d3e380510ac0e8da64126b6a2b88a32
Salvador Pereira <Ramon.Pereirae@gmail.com>
2023-01-05 17:10:48 -0600

    Collatz conjecture practice project using the highchart library in agular 15.0.2

--- src/app/aside/aside.component.ts
+++ src/app/aside/aside.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-aside',
@@ -6,6 +6,5 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./aside.component.css']
 })
 export class AsideComponent {
-
 
 }
--- src/app/highchart/highchart.component.ts
+++ src/app/highchart/highchart.component.ts
@@ -19,66 +19,6 @@ export class HighchartComponent {
     
     
     constructor(private hichartService: HighchartService){}
-    
- 
-  // chart: Chart = new Chart;
-
-  // ngOnInit() {
-  //   this.init();
-  // }
-
-  // addPoint() {
-  //   if (this.chart) {
-  //     this.chart.addPoint(Math.floor(Math.random() * 10));
-  //   } else {
-  //     alert('init chart, first!');
-  //   }
-  // }
-  // //Para agregar 
-  // addSerie() {
-  //   this.chart.addSeries({
-  //     name: 'Line',
-  //     type: 'line',
-  //     data: [22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1]},true,true);
-  // }
-
-
-  // removePoint() {
-  //   this.chart.removePoint(this.chart.ref.series[0].data.length - 1);
-  // }
-
-  // removeSerie() {
-  //   this.chart.removeSeries(this.chart.ref.series.length - 1);
-  // }
-
-  // init() {
-  //   let chart = new Chart({
-  //     chart: {
-  //       type: 'line',
-  //       height: 320
-  //     },
-  //     title: {
-  //       text: 'Collatz conjeture'
-  //     },
-  //     credits: {
-  //       enabled: false
-  //     },
-  //     series: [{
-  //       name: 'Line 1',
-  //       type: 'line',
-  //       data: [1, 2, 3]
-  //     }]
-  //   });
-
-  //   chart.addPoint(4);
-  //   this.chart = chart;
-  //   chart.addPoint(5);
-  //   setTimeout(() => {
-  //     chart.addPoint(6);
-  //   }, 2000);
-
-  //   chart.ref$.subscribe(console.log);
-  // }
   }
 
 
